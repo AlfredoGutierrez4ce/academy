e749a62a4f457c68b636cf3c87350ee38a63b51a
AlfredoGutierrez4ce <alfredo.gutierrez@4ce.com.mx>
2022-03-11 16:23:57 +0000

    [ADD] Basic Module

---
+++ odoo_space/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Space mission',
+    
+    'sumary': """App to manage Space mission to the moon""",
+    
+    'description': """
+       Space mission Module to manage mission to the moon
+       - Spaceship
+       - Spacecrew
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Manage',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+
+    ],
+    
+    'demo': [
+        
+    ],
+}
