9d2895d4c0d473a148505a2acb520b6f4294f51d
AlfredoGutierrez4ce <alfredo.gutierrez@4ce.com.mx>
2022-03-15 17:30:08 +0000

    [ADD] Basic Module

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -19,11 +19,14 @@
     'category': 'Training',
     'version': '0.1',
     
-    'depends': ['base'],
+    'depends': ['sale'],
     
     'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml', 
     ],
     
     'demo': [
